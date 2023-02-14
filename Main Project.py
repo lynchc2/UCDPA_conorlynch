7eb9160dd55ac2097aeb0a966183315de068a137
LynchC2 <conor.l.lynch@ppb.com>
2023-02-14 18:20:29 +0000

    Update

--- Main Project.py
+++ Main Project.py
@@ -4,4 +4,7 @@ import pandas as pd
 data = pd.read_csv("C:/Users/lynchc2/OneDrive - Paddy Power Betfair/Conor Lynch/UCD Course/Hotel Reservations.csv")
 
 print (data.describe())
+print (data.head())
+
+print (data.isna().sum())
 
