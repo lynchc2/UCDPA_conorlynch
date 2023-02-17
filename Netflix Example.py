9537a0a07fac99560524abf9c944cd1dd91d8350
LynchC2 <conor.l.lynch@ppb.com>
2023-02-17 14:33:25 +0000

    Update

--- Netflix Example.py
+++ Netflix Example.py
@@ -8,6 +8,7 @@ df = pd.read_csv("C:/Users/lynchc2/OneDrive - Paddy Power Betfair/Conor Lynch/UC
 print (df.describe())
 print (df.head())
 print (df.isna().sum())
+print (df.info())
 
 #Replacing nulls
 df['imdb_score'] = df['imdb_score'].fillna(df['imdb_score'].median())
@@ -15,8 +16,26 @@ df['title'].replace(np.nan, 'No Data',inplace  = True)
 
 #Drop duplicate rows
 df.drop_duplicates(inplace= True)
+print (df.isna().sum().sort_values(ascending=False))
 
-print (df.isna().sum())
+#Drop 1st column which is just an id
+df=df.drop('id',axis=1)
+print (df.head())
+
+#Regex and function to find patterns
+regex = (r'\d+')
+title_list = df['description'].astype(str)
+
+for i in title_list:
+    #Find all matches of regex in each title
+    cowboy = re.findall(regex, i)
+
+    print ("Numbers contrained in this description{}".format(cowboy))
+
+
+
+
+
 
 
 
