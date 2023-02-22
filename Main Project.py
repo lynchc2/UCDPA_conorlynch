418b4665ada0f05043941ad0dcb7259b1731963f
LynchC2 <conor.l.lynch@ppb.com>
2023-02-22 15:08:51 +0000

    Update

--- Main Project.py
+++ Main Project.py
@@ -1,12 +1,18 @@
 import numpy as np
 import pandas as pd
+#pd.set_option('display.max_columns', None)
 
 data = pd.read_csv("C:/Users/lynchc2/OneDrive - Paddy Power Betfair/Conor Lynch/UCD Course/Hotel Reservations.csv")
 
 print (data.describe())
 print (data.head())
+print (data.info())
+
+#Checking for NULLs
 print (data.isna().sum())
 
 #Drop duplicate rows
 data.drop_duplicates(inplace= True)
+
+
 
